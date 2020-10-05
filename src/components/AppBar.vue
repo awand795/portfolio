c7826d2e8fbf7d7ca0689abac36336f9ce0131bf
awand795 <awand795@gmail.com>
2020-10-05 16:31:31 +0700

    First Commit

---
+++ src/components/AppBar.vue
@@ -0,0 +1,50 @@
+<template>
+    
+    <v-app-bar
+      hide-on-scroll
+      absolute
+      color="transparent"
+      flat
+      height="120px"
+      dark
+    >
+    <v-layout justify-center>
+      <v-container>
+        <v-layout>
+            <div class="d-flex align-center">
+                <p class="font-weight-bold display-1">Awanda</p>
+            </div>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+                @click="$vuetify.goTo('#aboutme',options )"
+                text
+            >
+                <span>About Me</span>
+            </v-btn>
+
+             <v-btn
+                @click="$vuetify.goTo('#myproject',options )"
+                text
+            >
+                <span>My Project</span>
+            </v-btn>
+
+             <v-btn
+                @click="$vuetify.goTo('#contactme',options )"
+                text
+            >
+                <span>Contact Me</span>
+            </v-btn>
+        </v-layout>
+      </v-container>
+    </v-layout>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <v-img
+      src="../assets/background3.jpg"
+      dark
+      max-height="500px"
+    >
+      <v-layout fill-height align-center>
+        <v-container>
+          <v-row align="center" justify="center" class="white--text">
+            <v-col cols="12">
+              <div
+                :class="{'display-2 font-weight-bold ': $vuetify.breakpoint.smAndUp, 'title font-weight-bold': $vuetify.breakpoint.smAndDown}"
+              >Welcome To My Page</div>
+              <div
+                :class="{'headline': $vuetify.breakpoint.smAndUp, 'subtitle-1': $vuetify.breakpoint.smAndDown}"
+              >Look At My Project</div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-layout>
+    </v-img>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <h2 id="aboutme">About Me</h2>
+          <p>Hi my name Awanda, i am 20 years old and i like programming, i just frontend developer love react and vue just
+            and backend developer using node js, php (Laravel) and deno js
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '../components/HelloWorld'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
